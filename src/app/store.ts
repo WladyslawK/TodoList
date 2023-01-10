ea185056e6d5db59fb93412509c7c37e9c677de6
WladyslawK <vladkurr7890@gmail.com>
2023-01-10 23:45:33 +0100

    feat (Store)  configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,6 +5,7 @@ import thunk, {ThunkDispatch} from "redux-thunk";
 import {useDispatch} from "react-redux";
 import {appReducer} from "./app-reducer";
 import {authReducer} from "../features/login/auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 export const rootReducer = combineReducers({
     listsReducer: todoListsReducer,
@@ -15,7 +16,9 @@ export const rootReducer = combineReducers({
 
 export type rootReducerType = ReturnType<typeof rootReducer>
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({
+    reducer: rootReducer
+})
 
 export type ThunkAppDispatchType = ThunkDispatch<rootReducerType, any, AnyAction>
 
